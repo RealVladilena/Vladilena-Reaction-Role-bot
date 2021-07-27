f97bdf9897dda155d1f4bce182b5b2081ba54e5a
Alice <Alice@LAPTOP-GALHPM23>
2021-07-27 19:51:38 +0200

    Ajoutez des fichiers projet.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Alice-Reaction-Role-bot
+
+Un simple bot qui donne des rôles quand on clique sur une reaction ^^
+
+## Avant toute chose !
+
+Installer bien [Discord JS](https://discord.js.org/#/) sur votre projet et [Node JS](https://nodejs.org/en/) sinon le bot de marchera jamais !!
+
+### Prérequis
+
+- Un bot avec un token [Discord Developper Application](https://discord.com/developers/applications/726766282354982912/information) (oui oui, je vous jure ^^)
+- Discord JS = [Discord JS](https://discord.js.org/#/)
+- Node JS = [Node JS](https://nodejs.org/en/)
+
+### 1er démarrage
+
+Aller dans le fichier [/config.json](https://github.com/SaabniaTv/Alice-Radio-Bot-Discord-2/blob/main/config.json), et remplacer "token" par votre bot Token !
+
+-> Dans un terminal noter = ``node main``(comme dab UwU)
+
+## Config
+Changez tous les placeholders du fichier ``config.json``.:
+ - `token`: Ton bot token
+ - `prefix`: Le prefix du bot
+
+## Autheur
+
+* **Alice** _alias_ [@SaabniaTv](https://github.com/SaabniaTv) UwU
---
+++ main.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        fetchAllMembers: true,
+        partials: ['MESSAGE', 'REACTION']
+    }),
+    config = require('./config.json'),
+    fs = require('fs')
+
+client.login(config.token)
+client.commands = new Discord.Collection()
+
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+
+client.on('messageReactionAdd', (reaction, user) => {
+    if (!reaction.message.guild || user.bot) return
+    const reactionRoleElem = config.reactionRole[reaction.message.id]
+    if (!reactionRoleElem) return
+    const prop = reaction.emoji.id ? 'id' : 'name'
+    const emoji = reactionRoleElem.emojis.find(emoji => emoji[prop] === reaction.emoji[prop])
+    if (emoji) reaction.message.guild.member(user).roles.add(emoji.roles)
+    else reaction.users.remove(user)
+})
+
+client.on('messageReactionRemove', (reaction, user) => {
+    if (!reaction.message.guild || user.bot) return
+    const reactionRoleElem = config.reactionRole[reaction.message.id]
+    if (!reactionRoleElem || !reactionRoleElem.removable) return
+    const prop = reaction.emoji.id ? 'id' : 'name'
+    const emoji = reactionRoleElem.emojis.find(emoji => emoji[prop] === reaction.emoji[prop])
+    if (emoji) reaction.message.guild.member(user).roles.remove(emoji.roles)
+})
+
+client.on('ready', () => {
+    const statuses = [
+        () => `by Alice | https://github.com/SaabniaTv`,
+        () => `${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)} utilisateurs`
+    ]
+    let i = 0
+    setInterval(() => {
+        client.user.setActivity(statuses[i](), { type: 'WATCHING' })
+        i = ++i % statuses.length
+    }, 1e4)
+})
+
+console.log(`TransCom Reaction Role est en ligne !`);
